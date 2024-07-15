4ce6e9551aca26f0040ca6ceb2f8e39894c1ff5c
harshalpendse <harshalpendse2142@gmail.com>
2024-07-15 10:10:59 +0530

    install react js and tailwind css and create the first template

---
+++ client/src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function App() {
+  return (
+    <div>
+      <h1 className='text-3xl text-red-200'>
+        Hello, This is Harshal from Thane.      </h1>
+    </div>
+  )
+}
